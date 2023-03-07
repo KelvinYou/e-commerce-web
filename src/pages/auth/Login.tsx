dc6deee968eb3a46b749cb51d001b551dccc09b8
KelvinYou <ykekelvin0220@gmail.com>
2023-03-07 08:48:37 +0800

    i18n

--- src/pages/auth/Login.tsx
+++ src/pages/auth/Login.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
+// import LocaleDropDown from '../../components/i18n/LocaleDropDown';
 
 const Login = () => {
+  const { t } = useTranslation();
+
   return (
-    <div>Login</div>
+    <div>
+      {/* <LocaleDropDown/> */}
+      <div>{t("Login to your account")}</div>
+      <div>{t("Email")}</div>
+      <div>{t("Password")}</div>
+      <div>{t("Login now")}</div>
+      <div>{t("Don't Have An Account ?")} <span>{t("Register")}</span></div>
+    </div>
   )
 }
 
